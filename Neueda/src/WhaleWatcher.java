5073b93cbe560259200b47e9b3cea51f1af23dab
BELFASTMET\cal16135681 <cal16135681@belfastmet.ac.uk>
2018-12-07 15:07:23 +0000

    no message

---
+++ Neueda/src/WhaleWatcher.java
@@ -0,0 +1,20 @@
+
+public class WhaleWatcher {
+
+	public static void main(String[] args) {
+		Whale rightWhale = new Whale("Right", "Atlantic", 2001, 21, 16);
+		Whale blueWhale = new Whale ("Blue", "Pacific", 2001, 23, 16);
+		Whale spermWhale = new Whale ("Sperm", "Atlactic", 1900, 20, 40);
+		Whale humpbackWhale = new Whale ("Humpback", "Antarctic", 919, 13, 13);
+		
+		
+		ArrayList<Whale> allwhales = new ArrayList<Whale>() {
+			add(rightWhale);
+			add(blueWhale);
+			add(spermWhale);
+			add(humpbackWhale);
+		};
+		
+	}
+
+}
